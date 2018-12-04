1c15f92f74ac5e9e012fe47b79ac2adccc3b41ae
benqgg <bbarret93@gmail.com>
2018-12-04 12:50:37 +0100

    daemon gulps call replacement - todo 476

--- src/common/perf_timer.cpp
+++ src/common/perf_timer.cpp
@@ -158,7 +158,7 @@ LoggingPerformanceTimer::~LoggingPerformanceTimer()
 	pause();
 	performance_timers->pop_back();
 	char s[12];
-	GULPS_PRINTF("{}{}{}{}",s , sizeof(s), "%8llu  ", (unsigned long long)(ticks_to_ns(ticks) / (1000000000 / unit)));
+	GULPS_PRINTF("{} {} {}",s , sizeof(s), (unsigned long long)(ticks_to_ns(ticks) / (1000000000 / unit)));
 	size_t size = 0;
 	for(const auto *tmp : *performance_timers)
 		if(!tmp->paused || tmp == this)
